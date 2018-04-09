c045562fd45ade95359384ff198564d1eaedbbc5
Akshat Agrawal <akshat@levy-ve504-0235.apn.wlan.upenn.edu>
2018-04-08 23:35:22 -0400

    lol

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -14,12 +14,13 @@ public class NetworkManager : MonoBehaviour {
   
 	/** Server endpoints **/ 
 	public string address = "http://multiplayar.me";
+	public string pointPollEndpoint = "/pointpoll";
 	public string anchorEndpoint  = "/anchor";
 	public string objectEndpoint = "/object";
 	public string imageEndpoint = "/image";
 	public string syncEndpoint = "/sync";
   
-  public bool DEBUG = false;
+	public bool DEBUG = false;
 
 	public Dictionary<string, GameObject> objectMap = new Dictionary<string, GameObject>();
 	public Dictionary<int, string> reverseObjectMap = new Dictionary<int, string> ();
@@ -31,7 +32,8 @@ public class NetworkManager : MonoBehaviour {
 	public List<GameObject> targets;
 	public List<bool> targetselected;
 	private Dictionary<Vector2, Vector3> hitPointMap = new Dictionary<Vector2, Vector3> ();
-					
+	private Dictionary<Vector3, Vector2> reverseHitPointMap = new Dictionary<Vector3, Vector2> ();
+
 	private bool anchorSelect = false;
 	private bool anchorChosen = false;
 
@@ -41,15 +43,14 @@ public class NetworkManager : MonoBehaviour {
 	private GameObject newCube;
 	private int frames;
 	private bool lockPoints = false;
-  
-  private string syncButtonGameObjectName = "SyncButton";
-  private bool readyToSync = false;
+	private bool pollForAnchor = false; 
+
+	private string syncButtonGameObjectName = "SyncButton";
+	private bool readyToSync = false;
 
 	private Sprite unselected;
 	private Sprite selected;
 	private Vector3[] pointClouds;
-
-	// private string pointformat = "{0:0.000},{1:0.000}";
 
 
 	[System.Serializable] 
@@ -60,6 +61,10 @@ public class NetworkManager : MonoBehaviour {
 		public PointInformation(float x, float y) {
 			this.x = x;
 			this.y = y;
+		}
+
+		public static PointInformation CreateFromJSON (string j) {
+			return JsonUtility.FromJson<PointInformation> (j);
 		}
 	}
 
@@ -80,10 +85,6 @@ public class NetworkManager : MonoBehaviour {
 		public string id;
 	}
 
-//	[System.Serializable]
-//	public class TransformData {
-//		public List<VectorInformation> v;
-//	}
 
 	[System.Serializable]
 	public class RootObject {
@@ -112,29 +113,30 @@ public class NetworkManager : MonoBehaviour {
 		anchorSelect = true;
 		targetselected = new List<bool> ();
 		UnityARSessionNativeInterface.ARFrameUpdatedEvent += ARFrameUpdated;
-    // find the button (if it exists)
-    setButtonText("Wait");
+    	// find the button (if it exists)
+  	  	setButtonText("Wait");
 	}
   
-  public void setButtonText(string setText) {
-    GameObject button = GameObject.Find(syncButtonGameObjectName);
-    if (button != null) {
-      Text text = button.GetComponentInChildren<Text>(true);
-      if (text != null) {
-        text.text = setText;
-      } else {
-        Debug.Log("Button has no text component!");
-      }
-    } else {
-      Debug.Log("No sync button found!");
-    }
-  }
+  	public void setButtonText(string setText) {
+	    GameObject button = GameObject.Find(syncButtonGameObjectName);
+	    if (button != null) {
+	      	Text text = button.GetComponentInChildren<Text>(true);
+	      	if (text != null) {
+	        	text.text = setText;
+	      	} else {
+	        	Debug.Log("Button has no text component!");
+	      	}
+	    } else {
+	      	Debug.Log("No sync button found!");
+	    }
+  	}
 
 	public void ARFrameUpdated(UnityARCamera camera) {
-    if (readyToSync == false) {
-      readyToSync = true;
-      setButtonText("Sync");
-    }
+	    if (readyToSync == false) {
+	      readyToSync = true;
+	      setButtonText("Sync");
+	    }
+
 		if (!lockPoints) {
 			pointClouds = camera.pointCloudData;
 		}
@@ -145,15 +147,13 @@ public class NetworkManager : MonoBehaviour {
 		if (frames % 10 == 0) {
 			SyncWorld ();
 			frames = 0;
+			if (pollForAnchor) {
+				StartCoroutine(requestAnchorPoint ());
+			}
 		}
 
 		if (!anchorSelect) {
 			foreach (GameObject t in targets) {
-//				Vector3 screenpos = mainCamera.GetComponentInChildren<Camera> ().WorldToScreenPoint(pos);
-//				int dim = 64;
-//				float x = screenpos.x;
-//				float y = screenpos.y;
-//				GUI.DrawTexture (new Rect(x, y, dim, dim), unselected, ScaleMode.ScaleToFit, true);
 				t.transform.LookAt(mainCamera.transform, -Vector3.up);
 			}
 
@@ -162,8 +162,6 @@ public class NetworkManager : MonoBehaviour {
 				Ray ray = Camera.main.ScreenPointToRay( Input.GetTouch(0).position );
 				RaycastHit hit;
 			
-//				&& hit.transform.gameObject.name.StartsWith("target")
-
 				if ( Physics.Raycast(ray, out hit) && hit.transform.gameObject.name.StartsWith("target") )
 				{
 					int index; 
@@ -176,8 +174,8 @@ public class NetworkManager : MonoBehaviour {
 						hit.transform.gameObject.GetComponent<SpriteRenderer> ().sprite = selected; 
 						anchorChosen = true;
 						anchor = hit.transform.gameObject;
-						SendOffset ();
-						// TODO: unselect the rest
+						SendSelectedAnchor2D ();
+
 						for (int i = 0; i < targets.Count; i++) {
 							GameObject other = targets [i];
 							if (i != index) {
@@ -186,9 +184,7 @@ public class NetworkManager : MonoBehaviour {
 						}
 						targetselected [index] = true;
 					}
-
 				}
-
 			}
 		}
 	}
@@ -229,6 +225,7 @@ public class NetworkManager : MonoBehaviour {
 			Vector3 testPoint = pointClouds [i];
 			Vector3 screenPos = tempCam.WorldToScreenPoint (testPoint);
 			hitPointMap.Add (new Vector2(screenPos.x, screenPos.y), testPoint);
+			reverseHitPointMap.Add (testPoint, new Vector2(screenPos.x, screenPos.y));
 			PointInformation fakePoint = new PointInformation(screenPos.x, screenPos.y);
 			Debug.Log ("making a fake point");
 			drawServerPoint (fakePoint, i);
@@ -237,17 +234,15 @@ public class NetworkManager : MonoBehaviour {
   }
 
 	public IEnumerator captureCameraView() {
-
-
 		int height = Screen.height;
 		int width = Screen.width;
 
-    // DEBUG code below that chooses the first 4 AR points and uses them to the sync up
-    // to the rest of the existing multiplayar system
+	    // DEBUG code below that chooses the first 4 AR points and uses them to the sync up
+	    // to the rest of the existing multiplayar system
 		if (DEBUG == true) {
-      debugServerPoints(height, width);
-      yield break;
-    }
+	      debugServerPoints(height, width);
+	      yield break;
+	    }
 
 		// create a texture to render the camera's view to
 		RenderTexture texture = new RenderTexture (width, height, 24);
@@ -273,18 +268,14 @@ public class NetworkManager : MonoBehaviour {
 
 		// send this image to a server
 		Debug.Log("Sending to the server");
-//		WWWForm form = new WWWForm();
 		byte[] raw_image_bytes;
 		raw_image_bytes = savedTexture.EncodeToPNG();
 		Debug.Log ("Writing " + raw_image_bytes.Length);
-//		form.AddBinaryData ("image", raw_image_bytes);
 		Dictionary<string, string> headers = new Dictionary<string, string> ();
 		headers.Add ("x-user-id", guid);
 		headers.Add ("x-dummy-id", "fucapplr");
 		headers.Add ("x-points", serializeARKitPoints());
 
-//		form.AddField("image", System.Convert.ToBase64String(raw_image_bytes));
-//		form.AddField ("id", "1"); 
 		WWW w = new WWW (address + imageEndpoint, raw_image_bytes, headers);
 		Debug.Log("Sent the request");
 		yield return w; 
@@ -296,14 +287,62 @@ public class NetworkManager : MonoBehaviour {
 			Debug.Log ("IMAGE POINTS RETRIEVED FROM THE SERVER");
 			MatchedPoints points = MatchedPoints.CreateFromJSON (w.text);
 			Debug.Log (points.points.Count);
-			int i = 0;
-			foreach (PointInformation point in points.points) {
-				Debug.Log ("POINT x: " + point.x + " y: " + point.y);
-				drawServerPoint (point, i);
-				i++;
+	
+			if (points.points.Count == 0) {
+				pollForAnchor = true;
+			} else {
+				int i = 0;
+				foreach (PointInformation point in points.points) {
+					Debug.Log ("POINT x: " + point.x + " y: " + point.y);
+					drawServerPoint (point, i);
+					i++;
+				}
 			}
 		}
 	}
+
+	public IEnumerator requestAnchorPoint() {
+		Dictionary<string, string> headers = new Dictionary<string, string> ();
+		headers.Add ("x-user-id", guid);
+		byte[] post_data = new byte[0];
+
+		WWW w = new WWW (address + pointPollEndpoint, post_data, headers);
+		Debug.Log("Sent the request");
+		yield return w; 
+
+		if (w.error != null) {
+			Debug.Log (w.error);
+		} else {
+			if (w.text.Length == 0) {
+				Debug.Log ("No anchor returned"); 
+			} else {
+				PointInformation anchorpoint = PointInformation.CreateFromJSON (w.text);
+				ARPoint arPoint = new ARPoint {
+					x = anchorpoint.x,
+					y = anchorpoint.y
+				};
+		
+				// prioritize reults types
+				ARHitTestResultType[] resultTypes = {
+					ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent, 
+					// if you want to use infinite planes use this:
+					//ARHitTestResultType.ARHitTestResultTypeExistingPlane,
+					ARHitTestResultType.ARHitTestResultTypeHorizontalPlane, 
+					ARHitTestResultType.ARHitTestResultTypeFeaturePoint
+				}; 
+		
+				foreach (ARHitTestResultType resultType in resultTypes)
+				{
+					if (HitTestWithResultType (arPoint, resultType))
+					{
+						pollForAnchor = false;
+						yield break;
+					}
+				}
+			}
+		}
+	}
+
 
 	public string serializeARKitPoints() {
 		lockPoints = true;
@@ -311,12 +350,12 @@ public class NetworkManager : MonoBehaviour {
 		Camera cam = mainCamera.GetComponentInChildren<Camera> ();
 		foreach (Vector3 point in pointClouds) {
 			Vector3 screenPos = cam.WorldToScreenPoint (point);
-//			string key = string.Format (pointformat, screenPos.x, screenPos.y);
-      string pointStringX = string.Format ("{0:0.000}", screenPos.x);
-      string pointStringY = string.Format ("{0:0.000}", screenPos.y);
-      string pointString = string.Format("{0},{1};", pointStringX, pointStringY);
+	      	string pointStringX = string.Format ("{0:0.000}", screenPos.x);
+	      	string pointStringY = string.Format ("{0:0.000}", screenPos.y);
+	      	string pointString = string.Format("{0},{1};", pointStringX, pointStringY);
 			serialized += pointString;
 			hitPointMap.Add (new Vector2(float.Parse(pointStringX), float.Parse(pointStringY)), point);
+			reverseHitPointMap.Add (point, new Vector2(float.Parse(pointStringX), float.Parse(pointStringY)));
 		}
 		lockPoints = false;
 		return serialized.Substring(0, serialized.Length - 1);
@@ -325,10 +364,9 @@ public class NetworkManager : MonoBehaviour {
 
 	public void drawServerPoint(PointInformation point, int index) {
 		Vector2 pointVector = new Vector2 (point.x, point.y);
-//		string key = string.Format (pointformat, pointVector.x, pointVector.y);
 		Vector3 anchorposition; 
 
-    bool matched = false;
+   		bool matched = false;
 		foreach (KeyValuePair<Vector2, Vector3> val in hitPointMap) {
 			Vector2 mapPoint = val.Key;
 			if (mapPoint.Equals (pointVector)) {
@@ -354,109 +392,59 @@ public class NetworkManager : MonoBehaviour {
 			}
 		}
     
-    if (!matched) {
-        Debug.Log("No local match found for the server point");
-        Debug.Log(pointVector);
-    }
-
-//		if (hitPointMap.TryGetValue (key, out anchorposition)) {
-//			matchpoints.Add (anchorposition);
-//			GameObject target = new GameObject ();
-//			target.name = "target" + index;
-//			SpriteRenderer r = target.AddComponent<SpriteRenderer> ();
-//			target.AddComponent<BoxCollider> ();
-//			targetselected.Add (false);
-//			r.sprite = unselected;
-//			r.enabled = true;
-//			target.transform.position = anchorposition;
-//			target.transform.localScale = new Vector3 (0.02f, 0.02f, 0.02f);
-//			targets.Add (target);
-//		} else {
-//			Debug.Log ("didn't find any matches in the hitpointmap");
-//			foreach (KeyValuePair<string, Vector3> val in hitPointMap) {
-//				Debug.Log (val.Key);
-//				Debug.Log (key.Equals (val.Key));
-//			}
-//		}
+	    if (!matched) {
+	        Debug.Log("No local match found for the server point");
+	        Debug.Log(pointVector);
+	    }
 
 		return;
 	}
-//		var screenPosition = Camera.main.ScreenToViewportPoint(pointVector);
-//		ARPoint arPoint = new ARPoint {
-//			x = screenPosition.x,
-//			y = screenPosition.y
-//		};
-//
-//		// prioritize reults types
-//		ARHitTestResultType[] resultTypes = {
-//			ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent, 
-//			// if you want to use infinite planes use this:
-//			//ARHitTestResultType.ARHitTestResultTypeExistingPlane,
-//			ARHitTestResultType.ARHitTestResultTypeHorizontalPlane, 
-//			ARHitTestResultType.ARHitTestResultTypeFeaturePoint
-//		}; 
-//
-//		foreach (ARHitTestResultType resultType in resultTypes)
-//		{
-//			if (HitTestWithResultType (arPoint, resultType, index))
-//			{
-//				return;
-//			}
-//		}
-		// if it failed, try again
-//		drawServerPoint (point);
-//	}
 
-//	bool HitTestWithResultType (ARPoint point, ARHitTestResultType resultTypes, int index)
-//	{
-//		List<ARHitTestResult> hitResults = UnityARSessionNativeInterface.GetARSessionNativeInterface ().HitTest (point, resultTypes);
-//		Debug.Log ("FAKE NEWS");
-//		Debug.Log (hitResults.Count);
-//		if (hitResults.Count > 0) {
-//			foreach (var hitResult in hitResults) {
-////				m_HitTransform.position = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
-////				m_HitTransform.rotation = UnityARMatrixOps.GetRotation (hitResult.worldTransform);
-//				Vector3 position = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
-//				matchpoints.Add (position);
-//				GameObject target = new GameObject ();
-//				target.name = "target" + index;
-//				SpriteRenderer r = target.AddComponent<SpriteRenderer> ();
-//				target.AddComponent<BoxCollider> ();
-//				targetselected.Add (false);
-//				r.sprite = unselected;
-//				r.enabled = true;
-//				target.transform.position = position;
-////				target.transform.localRotation = Quaternion.LookRotation (mainCamera.transform.position - position);
-//				target.transform.localScale = new Vector3 (0.02f, 0.02f, 0.02f);
-//				targets.Add (target);
-////				Debug.Log (string.Format ("FAKE POINT x:{0:0.######} y:{1:0.######} z:{2:0.######}", m_HitTransform.position.x, m_HitTransform.position.y, m_HitTransform.position.z));
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
 
-  
-	/*
-	 */
-	public void SendOffset() {
+	bool HitTestWithResultType (ARPoint point, ARHitTestResultType resultTypes)
+	{
+		List<ARHitTestResult> hitResults = UnityARSessionNativeInterface.GetARSessionNativeInterface ().HitTest (point, resultTypes);
+		Debug.Log (hitResults.Count);
+		if (hitResults.Count > 0) {
+			foreach (var hitResult in hitResults) {
+//				m_HitTransform.position = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
+//				m_HitTransform.rotation = UnityARMatrixOps.GetRotation (hitResult.worldTransform);
+				Vector3 position = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
+				GameObject target = new GameObject ();
+				SpriteRenderer r = target.AddComponent<SpriteRenderer> ();
+				target.AddComponent<BoxCollider> ();
+				r.sprite = selected;
+				r.enabled = true;
+				target.transform.position = position;
+				target.transform.localScale = new Vector3 (0.02f, 0.02f, 0.02f);
+
+
+				// anchor selected on server! LOCK IN
+				anchorChosen = true;
+				anchorSelect = true;
+				anchor = target;
+
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void SendSelectedAnchor2D() {
 		if (synced) {
 			return;
 		}
+		
+		Vector3 selectedPosition3D = anchor.transform.position;
+		Vector2 selectedPosition2D;
+		reverseHitPointMap.TryGetValue (selectedPosition3D, out selectedPosition2D);
+		Dictionary<string, string> headers = new Dictionary<string, string> ();
+		headers.Add ("x-user-id", guid);
+		headers.Add ("x-xcord", selectedPosition2D.x.ToString());
+		headers.Add ("x-ycord", selectedPosition2D.y.ToString()); 
+		byte[] post_data = new byte[0];
 
-		Vector3 offset = OffsetFromAnchor (mainCamera);
-
-//		Debug.Log (string.Format ("Offset: x:{0:0.######} y:{1:0.######} z:{2:0.######}", offset.x, offset.y, offset.z));
-//		Debug.Log (offset);
-
-		WWWForm form = new WWWForm();
-		form.AddField("x", offset.x.ToString());
-		form.AddField("y", offset.y.ToString());
-		form.AddField("z", offset.z.ToString());
-		if (userId.Length != 0) {
-			form.AddField("userId", userId);
-		}
-
+		// generate a local cube to fuck with
 		newCube = Instantiate (hitCubePrefab, new Vector3 (0, 0, 0), Quaternion.identity);
 		newCube.SetActive (true);
 		anchor.SetActive (false);
@@ -466,7 +454,7 @@ public class NetworkManager : MonoBehaviour {
 
 		synced = true;
 
-		WWW www = new WWW(address + anchorEndpoint, form);
+		WWW www = new WWW(address + anchorEndpoint, post_data, headers);
 		StartCoroutine(WaitForUserId(www));
 	}
 
@@ -475,31 +463,30 @@ public class NetworkManager : MonoBehaviour {
 	 * signals the server that this is a brand new object.
 	 */
 	public void SendObject(string objectId, GameObject obj) {
-
-//		Vector3 offset = OffsetFromAnchor (obj);
 		GameObject temp = new GameObject ();
 		temp.transform.position = obj.transform.position;
 		temp.transform.RotateAround (anchor.transform.position, Vector3.up, -1.0f * anchor.transform.rotation.eulerAngles.y);
 		temp.transform.position = temp.transform.position - anchor.transform.position;
 		Vector3 offset = temp.transform.position;
-//		Vector3 offset = obj.transform.position - anchor.transform.position;
 
 		Debug.Log ("Sending update for Object: " + objectId);
 		Debug.Log ("Local Position: " + obj.transform.position + " World Position: " + offset.ToString());
 
-		WWWForm form = new WWWForm ();
-		form.AddField("x", offset.x.ToString());
-		form.AddField("y", offset.y.ToString());
-		form.AddField("z", offset.z.ToString());
+
+		Dictionary<string, string> headers = new Dictionary<string, string> ();
 		if (objectId.Length != 0) {
-			form.AddField("objectId", objectId);
+			headers.Add ("x-object-id", objectId);
 		}
+		headers.Add ("x-xcord", offset.x.ToString ());
+		headers.Add ("x-ycord", offset.y.ToString ()); 
+		headers.Add ("x-zcord", offset.z.ToString ());
+		byte[] post_data = new byte[0];
 
-		WWW www = new WWW(address + objectEndpoint, form);
-		StartCoroutine(WaitForObjectId(www, obj));
+		WWW w = new WWW (address + objectEndpoint, post_data, headers);
+		Debug.Log("Sent the request");
+		StartCoroutine(WaitForObjectId(w, obj));
 	}
-
-
+		
 	/* Get state of the world from server
 	 */
 	public void SyncWorld() {
@@ -567,8 +554,6 @@ public class NetworkManager : MonoBehaviour {
 		foreach (VectorInformation v in root.objects) {
 			string objectId = v.id;
 			Vector3 otherPos = new Vector3 (v.x, v.y, v.z);
-
-
 
 			if (objectMap.ContainsKey (objectId)) {
 				GameObject obj = objectMap [objectId];
